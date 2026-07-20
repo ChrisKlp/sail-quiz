59431e6f30a1206cc1477054ff8ee42b0ddd0f8c
Krzysztof Klepadlo <klepadlo.krzysztof@gmail.com>
2026-07-20 21:04:56 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button';
+
+function App() {
+  return (
+    <div>
+      <p>Hello Vite + React!</p>
+      <Button>Hello</Button>
+    </div>
+  );
+}
+
+export default App;
